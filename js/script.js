3789a896f26ffc767219a81b24b8391a01c2bb8b
Ivan Qian <118160942+IQ2041@users.noreply.github.com>
2025-07-27 21:06:43 -0400

    checking the server side entry
    trying to fix the github page not running

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ toggle.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
 });
 
+
 const loginForm = document.getElementById('login-form');
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const errorMessage = document.getElementById('error-message');
 const welcomeUser = document.getElementById('welcome-user');
+
 
 loginForm?.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -28,6 +30,7 @@ loginForm?.addEventListener('submit', function (e) {
         
         //This will redirect it to the quiz page.
          window.location.href = './quiz.html'
+
     }
 });
 
